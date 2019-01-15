0a3ecbf3f030d991c5c0a1d8b2ace50b601697c4
Philip Daniels <Philip.Daniels1971@gmail.com>
2019-01-15 21:48:01 +0000

    Ensure we check for both newline chars when making a safe slice.

--- src/byte_extensions.rs
+++ src/byte_extensions.rs
@@ -104,8 +104,8 @@ impl ByteSliceExtensions for [u8] {
     /// Makes the slice safe to write to CSV by checking for any embedded '\r' characters
     /// and replacing them with a space. Uses a Cow to avoid allocaions in most cases.
     fn make_safe<'f>(&'f self) -> Cow<'f, [u8]> {
-        if self.contains(&b'\r') {
-            let safe: Vec<_> = self.iter().map(|&c| if c == b'\r' { b' ' } else { c }).collect();
+        if self.contains(&b'\r') || self.contains(&b'\n') {
+            let safe: Vec<_> = self.iter().map(|&c| if c == b'\r' || c == b'\n' { b' ' } else { c }).collect();
             safe.into()
         } else {
             self.into()
